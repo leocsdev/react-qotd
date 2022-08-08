befdd5b5d2a7ec012fa08013d403702ea6cfed5f
Leo <leocsdev@gmail.com>
2022-08-08 19:24:40 +0800

    Added Unsplash API for background image (temporary commented out)

--- src/App.js
+++ src/App.js
@@ -3,20 +3,39 @@ import './App.css'
 
 function App() {
   const [quotes, setQuotes] = useState([])
+  // const [image, setImage] = useState('')
 
   useEffect(() => {
     fetch('https://type.fit/api/quotes')
       .then((response) => response.json())
       .then((data) => setQuotes(data))
-      .catch(() => console.log('Something went wrong...'))
+      .catch(() => console.log('Something went wrong with quote fetching.'))
   }, [])
 
+  // useEffect(() => {
+  //   const options = {
+  //     method: 'GET',
+  //     headers: {
+  //       Authorization: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
+  //     },
+  //   }
+
+  //   fetch(
+  //     'https://api.unsplash.com//photos/random?orientation=landscape',
+  //     options
+  //   )
+  //     .then((response) => response.json())
+  //     .then((data) => setImage(data))
+  //     .catch(() => console.log('Something went wrong with image fetching.'))
+  // }, [])
+
+  // if (quotes.length === 0 || image === '') {
   if (quotes.length === 0) {
     return <p>Loading...</p>
   } else {
     const randomNumber = () => {
       const total = quotes.length
-      return Math.floor(Math.random() * total) + 1
+      return Math.floor(Math.random() * total)
     }
 
     const randomQuote = () => {
@@ -34,6 +53,12 @@ function App() {
           <h2>{quote.text}</h2>
           <footer>{quote.author || 'Unknown'}</footer>
         </blockquote>
+
+        <img
+          src="https://images.unsplash.com/photo-1579294800821-694d95e86143?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+          alt="From Unsplash"
+          style={{ width: '100vw' }}
+        />
       </div>
     )
   }
